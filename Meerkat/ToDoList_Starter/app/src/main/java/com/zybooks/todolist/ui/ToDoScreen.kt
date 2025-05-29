b4d75979fc7da4c0650e3ebc7f52eb4b54e76469
Frank McCown <fmccown@harding.edu>
2025-05-29 16:29:46 -0500

    Remove unused imports plus gradle changes

--- Meerkat/ToDoList_Starter/app/src/main/java/com/zybooks/todolist/ui/ToDoScreen.kt
+++ Meerkat/ToDoList_Starter/app/src/main/java/com/zybooks/todolist/ui/ToDoScreen.kt
@@ -1,8 +1,6 @@
 package com.zybooks.todolist.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,7 +9,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.SwipeToDismissBoxState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,7 +16,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
